c28c604aca58cfec97ab5f2f95ae468ec29f8943
Pedró Sìlvá <psmaranhao@gmail.com>
2024-12-10 14:46:47 +0100

    feat: Add deploy script and set base path for Vite configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/netflix-clone/",
   plugins: [react()],
   build: {
     outDir: "dist",
