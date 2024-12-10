aaeb60f1cd0c363bdcfdc813fa854d3e7d6b5a6c
Pedró Sìlvá <psmaranhao@gmail.com>
2024-12-10 13:54:10 +0100

    refactor: migrate to Vite and update file extensions to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Tmdb from './Tmdb'
 import MovieRow from './components/MovieRow';
